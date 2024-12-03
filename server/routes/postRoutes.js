2509078449b6f53c2c078df07bb27d9601011509
Abdullah al rafi <abdullah116632@gmail.com>
2024-12-03 15:40:05 +0600

    user routes complete

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,4 @@
+import express from "express";
+import userAuth from "../middleware/authMiddleware.js";
+
+const router = express.Router();
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import path from "path";
-import { changePassword, getUser, requestPasswordReset, resetPassword, updateUser, verifyEmail } from "../controllers/userController.js";
+import { acceptRequest, changePassword, friendRequest, getFriendRequest, getUser, profileViews, requestPasswordReset, resetPassword, suggestedFriends, updateUser, verifyEmail } from "../controllers/userController.js";
 import userAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -15,6 +15,19 @@ router.post("/reset-password", changePassword);
 // user routes
 router.post("/get-user/:id?", userAuth, getUser);
 router.put("/update-user", userAuth, updateUser);
+
+// friend request
+router.post("/friend-request", userAuth, friendRequest);
+router.post("/get-friend-request", userAuth, getFriendRequest);
+
+// accept / deny friend request
+router.post("/accept-request", userAuth, acceptRequest);
+
+// view profile
+router.post("/profile-view", userAuth, profileViews);
+
+//suggested friends
+router.post("/suggested-friends", userAuth, suggestedFriends);
 
 router.get("/verified", (req, res) => {
     res.sendFile(path.join(__dirname, "./views", "index.html"));
